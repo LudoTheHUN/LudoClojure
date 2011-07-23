c8ce88156f7773f67f4fb214e6bc46ea6b0be715
Ludo <ludoart@yahoo.com>
2011-07-24 00:24:07 +0100

    I think I have lain workig, with tests, jaring etc, woohoo. It still took too long and was too complicated, make it simpler people

--- src/LudoClojure/core.clj
+++ src/LudoClojure/core.clj
@@ -1,1 +1,13 @@
-(ns LudoClojure.core)
+(ns LudoClojure.core
+  (:gen-class))
+
+(defn -main [& args]
+  (println "you people dont make it easy to get started with programming"))
+
+
+(+ 4 5)
+(println "hello there from core.clj file")
+
+(defn hello
+  ([] "Hello world!")
+  ([name] (str "Hello " name "!")))
--- test/LudoClojure/test/core.clj
+++ test/LudoClojure/test/core.clj
@@ -2,5 +2,15 @@
   (:use [LudoClojure.core])
   (:use [clojure.test]))
 
+(comment
+This is the old values that was here....
 (deftest replace-me ;; FIXME: write
   (is false "No tests have been written."))
+)
+
+
+
+(deftest simple-test
+  (is (= (hello) "Hello world!"))
+  (is (= (hello "test") "Hello test!")))
+
