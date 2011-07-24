8e63a15f2161de9dda9a22c58d3b3ce942933e59
Ludo <ludoart@yahoo.com>
2011-07-24 02:16:20 +0100

    got mouse moving and recursion which fails if it goes too deeply

--- src/LudoClojure/core.clj
+++ src/LudoClojure/core.clj
@@ -1,13 +1,20 @@
 (ns LudoClojure.core
   (:gen-class))
 
-(defn -main [& args]
-  (println "you people dont make it easy to get started with programming"))
 
 
-(+ 4 5)
+
 (println "hello there from core.clj file")
 
 (defn hello
   ([] "Hello world!")
   ([name] (str "Hello " name "!")))
+
+
+(defn -main [& args]
+  (println "you people dont make it easy to get started with programming")
+(println (hello args))
+)
+
+
+
---
+++ src/LudoClojure/scrachsnippets.clj
@@ -0,0 +1,57 @@
+
+(comment Make Java move the mouse to x y location)
+(.mouseMove (java.awt.Robot.) 10 23)
+
+(comment lets see how quickly this can itterate)
+
+
+(def countingdown1 (fn [n]
+                    (println n)  n))
+(countingdown1 10)
+
+
+(def countingdown2 (fn [n]
+                     (println n) 
+                     (- n 1)))
+(countingdown2 10)
+
+(comment this may make it fun to run off)
+(defn countingdown3 (fn [n]
+                     (println n)
+                     (Thread/sleep 1000) 
+                     (.mouseMove (java.awt.Robot.) n n)
+                     (countingdown3 (- n 1) )))
+(countingdown3 500)
+
+
+(defn countingdown4 [n]
+  (if (< n 1) (println "all done")
+                     (do (println n)
+                      (comment (Thread/sleep 1) 
+                       (.mouseMove (java.awt.Robot.) n n))
+                       (countingdown4 (- n 1) ))))
+(countingdown4 50000)
+(comment what's with the stack overflow!!)
+
+
+(defn countingdown5 [n]
+  (loop (if (< n 1) (println "all done")
+                     (do (println n)
+                      (comment (Thread/sleep 1) 
+                       (.mouseMove (java.awt.Robot.) n n))
+                       (recur(countingdown5 (- n 1) ))))))
+(countingdown5 50000)
+(comment WIP this still fails)
+
+
+
+
+    
+(def factorial
+  (fn [n]
+    (loop [cnt n acc 1]
+       (if (zero? cnt)
+            acc
+          (recur (dec cnt) (* acc cnt))))))
+(factorial 10)      
+         
