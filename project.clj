4effc7244e2e8da7df35d786f4fe7e1f71127fe8
LudoTheHUN <ludoart@yahoo.com>
2011-09-01 21:35:57 +0000

    Edited project.clj via GitHub

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "This project is designed to be a practical walk through of all the pain points of writing a Clojure application, hopefully keeping track of aha moments relatd to namespaces, adding dependencies, libs, testing and Clojure specifics and benchmarking"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-
+  :jvm-opts ["-Xmx1024m"]
   :main LudoClojure.core )
